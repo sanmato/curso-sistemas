4010118c7589c0bc8201ea564fd2317976414dbc
sanmato <santiago.mato.rpc@gmail.com>
2020-08-21 18:16:42 -0300

    Sort LACTEOS

--- tema-4/script.js
+++ tema-4/script.js
@@ -43,7 +43,7 @@ let identificarLacteoDeMayorContenidoDeCalcio = (tabla_de_datos) => {
     tabla_de_datos.sort ((c1, c2) => (c1.contenido_calcio < c2.contenido_calcio) ? 1 : 
         (c1.contenido_calcio > c2.contenido_calcio) ? -1 : 0);
 
-    // Este otro metodo ordena la lista mediante la comparacion de valor de una de las propiedades
+    // Este otro metodo ordena la lista mediante la comparacion de valor de una de las propiedades, en este caso el contenido
 
     return tabla_de_datos[0].lacteo;
 }
